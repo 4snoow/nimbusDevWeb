99b939453ef50a4c673add3d214e03bf6a8d5cfb
Gabriel Bastos <gabribs1@hotmail.com>
2024-01-17 20:19:32 -0300

    Feat: Form submit

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Fade } from "react-awesome-reveal";
 import styled from "styled-components";
 
@@ -77,32 +78,104 @@ const Div = styled.div`
       }
     }
   }
+
+  > div form{
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+    width: auto;
+    height: auto;
+  }
 `
 
 function Form() {
-  return (  
-    
-    <Div>
-      <Fade triggerOnce={true} direction="left" cascade={true}>
-      <h2>Contato</h2>
-      <div className="form-fields">
-        <div>
-          <label htmlFor="name">Nome</label>
-          <input id="name" type="text" />
-        </div>
-        <div>
-          <label htmlFor="mail">E-mail</label>
-          <input id="mail" type="email" />
-        </div>
-        <div>
-          <label htmlFor="message">Mensagem</label>
-          <textarea name="Message" id="message"  cols="30" rows="10"></textarea>
-        </div>
-      </div>
-      <button id="btn-submit" type="submit">Enviar</button>
-    </Fade>
-    </Div>
-  );
+const [dataForm, setDataForm] = useState({
+  nome:'',
+  email:'',
+  message:'',
+  phone:'',
+  contacted:'',
+})
+
+const handleValue = (event) => {
+  setDataForm((dataForm) =>({
+    ...dataForm,[event.target.name]:event.target.value
+  }))
 }
+const handleForm = async (event) => {
+  event.preventDefault()
+ 
+  try {
+    const response = await fetch('http://localhost:300/enviar-form', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dataForm),
+    });
+
+    if (response.ok) {
+      console.log('Submissão do formulário enviada com sucesso.');
+
+    } else {
+      console.error('Erro ao enviar submissão do formulário.');
+    }
+  } catch (error) {
+    console.error('Erro ao enviar submissão do formulário:', error.message);
+  }
+};
+
+const handlePhone = (event) => {
+  let input = event.target
+  input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
+
+return (    
+  <Div>
+    <Fade triggerOnce={true} direction="left" cascade={true}>
+    <h2>Contato</h2>
+    <form action="" onSubmit={handleForm}>
+    <div className="form-fields">
+      <div>
+        <label htmlFor="name">Nome</label>
+        <input name="nome" id="name" type="text" onChange={handleValue} />
+      </div>
+      <div>
+        <label htmlFor="mail">E-mail</label>
+        <input name="email" id="mail" type="email" onChange={handleValue}/>
+      </div>
+      <div>
+        <label htmlFor="phone">WhatsApp</label>
+        <input name="phone" id="phone" type="tel" maxLength="15" onKeyUp={handlePhone}  onChange={handleValue}/>
+      </div>
+      {/* <div>
+        <label htmlFor="cnpj">WhatsApp</label>
+        <input name="cnpj" id="cnpj" type="" maxLength="15" onKeyUp={handlePhone}  onChange={handleValue}/>
+      </div> */}
+      <div>
+        <label htmlFor="contacted">Por onde nos conheceu</label>
+        <input name="contacted" id="contacted" type="text" placeholder="WhatsApp, Facebook, Twitter..." onChange={handleValue} />
+      </div>
+      <div>
+        <label htmlFor="message">Mensagem</label>
+        <textarea name="message" id="message"  cols="30" rows="10" onChange={handleValue}></textarea>
+      </div>
+    </div>
+    <button id="btn-submit" type="submit">Enviar</button>
+    </form>
+  </Fade>
+  </Div>
+);
+}
+ 
+
 
 export default Form;
